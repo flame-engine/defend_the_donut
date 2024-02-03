4feb0eecd191660fd3f198dc1d6761281724267b
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2024-02-03 09:33:06 +0100

    feat: allow mouse to correctly get the pointer delta

--- lib/mouse.dart
+++ lib/mouse.dart
@@ -1,21 +1,55 @@
+import 'dart:async';
 import 'dart:ui';
 
+import 'package:flutter/services.dart';
 import 'package:pointer_lock/pointer_lock.dart';
 
 class Mouse {
-  static final _lock = PointerLock();
-
-  static Future<void> init() => _lock.subscribeToRawInputData();
-
-  // TODO: I think we want to use startPointerLockSession instead
-  static Offset get delta {
-    _lock.lastPointerDelta().then((value) => _delta = value);
-    return _delta;
-  }
-
+  static Offset get delta => _delta;
   static Offset _delta = Offset.zero;
 
-  static Future<void> lock() => _lock.lockPointer();
+  static final _lock = PointerLock();
 
-  static Future<void> unlock() => _lock.unlockPointer();
+  static bool _pointerLocked = false;
+
+  static ValueChanged<PointerDataPacket>? _onPointerDataPacket;
+
+  static Future<void> init() async {
+    _onPointerDataPacket = PlatformDispatcher.instance.onPointerDataPacket!;
+
+    PlatformDispatcher.instance.onPointerDataPacket = (packet) async {
+      _onPointerDataPacket?.call(packet);
+
+      // If any of the data events is a move or hover we should get a new delta.
+      final hasPointerMoved = packet.data.any((e) =>
+          e.change == PointerChange.move || e.change == PointerChange.hover);
+
+      // If the data is empty and the pointer is locked then we can assume that
+      // the user did move the mouse because Flutter will still trigger the
+      // onPointerDataPacket but it cant retrieve data because the pointer was
+      // locked.
+      final synthesizeMovement = packet.data.isEmpty && _pointerLocked;
+
+      if (hasPointerMoved || synthesizeMovement) {
+        _delta = await _lock.lastPointerDelta();
+      }
+    };
+  }
+
+  static Future<void> dispose() async {
+    if (_onPointerDataPacket != null) {
+      PlatformDispatcher.instance.onPointerDataPacket = _onPointerDataPacket;
+    }
+    return await unlock();
+  }
+
+  static Future<void> lock() async {
+    if (_pointerLocked) return;
+    return _lock.lockPointer().then((_) => _pointerLocked = true);
+  }
+
+  static Future<void> unlock() async {
+    if (!_pointerLocked) return;
+    return _lock.lockPointer().then((_) => _pointerLocked = false);
+  }
 }
